5af7d7afee381c5845b8c2e2d609c3d1a4d38ad5
Kysonn Dela Cerna <kysonndelacerna@gmail.com>
2022-05-26 17:43:34 +0800

    it work

--- bot.py
+++ bot.py
@@ -56,9 +56,15 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    global players
     global game_started
-    global MAX_PLAYERS
+    global players
+    global czar
+    global current_prompt
+    global prompts_copy
+    global responses_copy
+    global waiting_for
+    global answers
+    global czar_chosen
     global channel
 
     if message.author == client.user:
@@ -83,7 +89,7 @@ async def on_message(message):
                     break
             
             if index == -1:
-                players.append((message.author.id, message.author.name, 0, []))
+                players.append([message.author.id, message.author.name, 0, []])
                 await message.add_reaction('\N{THUMBS UP SIGN}')
 
         elif arguments[1] == "leave":
@@ -102,7 +108,10 @@ async def on_message(message):
             if len(players) >= 3:
                 game_started = True
                 channel = message.channel
-                await start_game()
+                prompts_copy = deepcopy(PROMPTS)
+                responses_copy = deepcopy(RESPONSES)
+                draw_cards()
+                await select_prompt()
                 await message.add_reaction('\N{THUMBS UP SIGN}')
             else:
                 await message.channel.send("Not enough players to start the game.")
@@ -124,24 +133,22 @@ async def on_message(message):
             
     if str(message.channel.type) == "private":
         # 1. check if we're waiting input from user
-        if message.author.id not in waiting_for:
-            return
-        else:
+        if message.author.id in waiting_for:
             # 2. check if input is valid
             indices = message.content.split(", ")
             valid = True
 
             for i in range(len(indices)):
-                if not index[i].isdigit():
+                if not indices[i].isdigit():
                     valid = False
                     break
-                index[i] = int(index[i])
-                if index[i] < 1 or index[i] > HAND_SIZE:
+                indices[i] = int(indices[i])
+                if indices[i] < 1 or indices[i] > HAND_SIZE:
                     valid = False
                     break
             
             set_indices = set(indices)
-            if len(set_indices) != indices:
+            if len(set_indices) != len(indices):
                 valid = False
             
             if len(set_indices) != current_prompt.count("_"):
@@ -149,6 +156,7 @@ async def on_message(message):
 
             # 3. update game state
             if valid:
+                await message.channel.send("Got your answer.")
                 waiting_for.remove(message.author.id)
 
                 player_index = -1
@@ -165,6 +173,14 @@ async def on_message(message):
                         players[player_index][3].pop(index - 1)
                     
                     answers.append(answer)
+            
+            # 4. if not waiting for anyone else
+            if not waiting_for:
+                answer_text = ""
+                for i in range(len(answers)):
+                    answer_text += str(i + 1) + ". " + "\n".join(answers[i][0])
+                    answer_text += "\n"
+                await message_player(players[czar][0], "The players reponses were:\n" + answer_text)
         
         # 1. if czar and everyone already submitted
         if not waiting_for and message.author.id == players[czar][0]:
@@ -174,6 +190,30 @@ async def on_message(message):
             if index < 1 or index > len(answers):
                 return
             czar_chosen = index
+            winner_index = -1
+            for i in range(len(players)):
+                if answers[czar_chosen - 1][1] == players[i][0]:
+                    winner_index = i
+
+            if winner_index != -1:
+                if channel:
+                    await channel.send("The winner is " + players[i][1])
+                    await channel.send("The black card was: ```" + current_prompt + "```")
+
+                    await channel.send(players[i][1] + "'s answer was\n" + "\n".join(answers[czar_chosen - 1][0]))
+
+            # 2. check if game is over
+            czar = (czar + 1) % len(players)
+            answers = []
+
+            if winner_index != -1:
+                players[winner_index][2] += 1
+                if players[winner_index][2] >= POINTS_TO_WIN:
+                    await channel.send(players[winner_index][1] + " has won!")
+                    end_game()
+                else:
+                    draw_cards()
+                    await select_prompt()
 
 async def message_player(id, message):
     try:
@@ -182,79 +222,55 @@ async def message_player(id, message):
     except:
         print("Something went wrong when messaging player")
 
-async def start_game():
-    prompts_copy = deepcopy(PROMPTS)
-    responses_copy = deepcopy(RESPONSES)
+def draw_cards():
+    global players
+    global responses_copy
 
-    while game_started:
-        # 1. draw
-        for player in players:
-            while len(player[3]) < HAND_SIZE:
-                if len(responses_copy) < 1:
-                    responses_copy = deepcopy(RESPONSES)
-                index = randint(1, len(responses_copy) - 1)
-                player[3].append(responses_copy[index])
-                responses_copy.pop(index)
+    for player in players:
+        while len(player[3]) < HAND_SIZE:
+            if len(responses_copy) < 1:
+                responses_copy = deepcopy(RESPONSES)
+            index = randint(1, len(responses_copy) - 1)
+            player[3].append(responses_copy[index])
+            responses_copy.pop(index)
 
-        # 2. pick black card, inform everyone
-        if len(prompts_copy) < 1:
-            prompts_copy = deepcopy(PROMPTS)
-        prompt_index = randint(1, len(prompts_copy) - 1)
-        current_prompt = prompts_copy[prompt_index]
-        prompts_copy.pop(prompt_index)
+async def select_prompt():
+    global players
+    global prompts_copy
+    global current_prompt
 
-        for i in range(len(players)):
-            message_player(player[i][0], "The black card is", current_prompt)
+    if len(prompts_copy) < 1:
+        prompts_copy = deepcopy(PROMPTS)
+    prompt_index = randint(1, len(prompts_copy) - 1)
+    current_prompt = prompts_copy[prompt_index]
+    prompts_copy.pop(prompt_index)
 
-            if i == czar:
-                message_player(player[i][0], "You are the czar.")
-            else:
-                waiting_for.append(player[i][0])
-                card_text = ""
-                for j in range(HAND_SIZE):
-                    card_text += str(j + 1) + ". " + player[i][3][j]
-                    if j != HAND_SIZE - 1:
-                        card_text += "\n"
-                message_player(player[i][0], "You're cards are:\n" + card_text)
+    for i in range(len(players)):
+        await message_player(players[i][0], "The black card is ```" + current_prompt + "```")
 
-        # 3. wait until everyone picks
-        while waiting_for:
-            pass
-
-        # 4. wait until czar chooses winner
-        czar_chosen = 0
-
-        answer_text = ""
-        for i in range(len(answers)):
-            answer_text += str(i + 1) + ". " + answers[0].join("\n")
-            if i != len(answers - 1):
-                answer_text += "\n"
-        message_player(player[i][0], "The players reponses were:\n", answer_text)
-
-        while czar_chosen == 0:
-            pass
-
-        winner_index = -1
-        for i in range(len(players)):
-            if answers[czar_chosen - 1][1] == players[i][0]:
-                winner_index = i
-
-        if winner_index != -1:
-            if channel:
-                await channel.message("The winner is", players[i][1])
-                await channel.message("The black card was:", current_prompt)
-
-                await channel.message(players[i][1] + "'s answer was", answers[czar_chosen - 1][0].join("\n"))
-
-        # 5. check if game is over
-        czar = (czar + 1) % len(players)
-        if winner_index != -1:
-            players[winner_index][2] += 1
-            if players[winner_index][2] >= POINTS_TO_WIN:
-                channel.message(players[winner_index][1], "has won!")
-                end_game()
+        if i == czar:
+            await message_player(players[i][0], "You are the czar.")
+        else:
+            waiting_for.append(players[i][0])
+            card_text = ""
+            for j in range(HAND_SIZE):
+                card_text += str(j + 1) + ". " + players[i][3][j]
+                if j != HAND_SIZE - 1:
+                    card_text += "\n"
+            await message_player(players[i][0], "You're cards are:\n" + card_text)
 
 def end_game():
+    global game_started
+    global players
+    global czar
+    global current_prompt
+    global prompts_copy
+    global responses_copy
+    global waiting_for
+    global answers
+    global czar_chosen
+    global channel
+
     game_started = False
     players = []
     czar = 0
